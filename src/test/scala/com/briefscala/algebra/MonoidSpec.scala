99e511bd49788626d5e66dda01e05fa7b1b3b7d5
Yoel Garcia Diaz <yoel@ereactive.com>
2019-01-24 00:15:35 +0000

    add labels to tests

--- src/test/scala/com/briefscala/algebra/MonoidSpec.scala
+++ src/test/scala/com/briefscala/algebra/MonoidSpec.scala
@@ -1,22 +1,23 @@
 package com.briefscala.algebra
 
 import org.scalacheck.{Arbitrary, Properties}
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop._
 
 abstract class MonoidSpec[A, Op[X] <: Operation[X]](
   m: Monoid[A, Op], name: String)(implicit
   arb: Arbitrary[A]) extends Properties(name) {
 
   property("leftIdentity") = forAll { a: A =>
-    m.combine(m.zero, a) == a
+    m.combine(m.zero, a) =? a label s"a ia $a"
   }
 
   property("rightIdentity") = forAll { a: A =>
-    m.combine(a, m.zero) == a
+    m.combine(a, m.zero) =? a label s"a is $a"
   }
 
   property("associativity") = forAll { (a0: A, a1: A, a2: A) =>
-    m.combine(a0, m.combine(a1, a2)) == m.combine(m.combine(a0, a1), a2)
+    m.combine(a0, m.combine(a1, a2)) =? m.combine(m.combine(a0, a1), a2) label
+      s"a0 is $a0, a1 is $a1 and a2 is $a2"
   }
 }
 
